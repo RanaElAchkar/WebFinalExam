c0330135b5ae7dedf404605ef9d7733ca273e9ae
NourRida <nourrida174@gmail.com>
2024-05-07 13:19:00 +0300

    fixed db connection

--- BE/common/connect.php
+++ BE/common/connect.php
@@ -4,19 +4,21 @@ function VarExist($var){
         return $var;
     }else{
         header("location:../index.html");
+        exit; // Add exit to stop script execution after redirection
     }
 }
 
 function dbconnect(){
-$dbhost= "localhost";
-$dbname= "moviebuff";
-$dbuser= "root";
-$dbpass="";
-try {
-    $db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);		
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-} 
+    $dbhost= "localhost";
+    $dbname= "moviebuff";
+    $dbuser= "root";
+    $dbpass="";
+    try {
+        $db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);		
+        return $db; 
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    } 
 }
 ?>
